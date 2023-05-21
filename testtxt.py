55e6db37536366a3e6736eb9e9e5553043f20d95
kuraga <kenegesova.k@gmail.com>
2023-05-22 01:20:00 +0300

    add action

---
+++ testtxt.py
@@ -0,0 +1,13 @@
+import pytest
+
+def test_txt_fiels():
+    txt_files = ['maps/map.txt', 'screens/start.txt', 'screens/win.txt', 'screens/gameover.txt']
+
+    for file in txt_files:
+        with open(file, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+
+            assert len(lines) <= 24, f"File {file} has more than 24 lines."
+
+            for line in lines:
+                assert len(line.rstrip('\n')) <= 59, f"Line in file {file} exceeds the maximum length of 59 characters."
