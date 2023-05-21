19910b9e147c5f34ba92f23da5a8de36304e4c39
kuraga <kenegesova.k@gmail.com>
2023-05-21 20:30:11 +0300

    Characters and game field

---
+++ objects/field.py
@@ -0,0 +1,71 @@
+import curses
+
+class GameField:
+
+    def __init__(self, screen, file, color1, color2):
+        self.screen = screen
+        self.file = file
+        self.color_field = color1
+        self.color_lives = color2
+        
+        self.map_matrix()
+        self.init_field()
+        
+    def map_matrix(self):
+        self.matrix = []
+        with open(self.file, encoding='utf-8') as file:
+            for line in file:
+                self.matrix.append(list(line)[:-1])
+        
+    def init_field(self): #создание окон
+        hight, width = self.screen.getmaxyx() #размер консоли (локальные переменные)
+        m = self.matrix
+        self.mh, self.mw = len(m), len(m[0]) #размер матрицы (атрибуты)
+        
+        offset_y = (hight - self.mh) // 2 #координаты сдвига
+        offset_x = (width - self.mw) // 2 
+        
+        self.mapa = self.screen.derwin(self.mh, self.mw, offset_y, offset_x)
+        
+        self.mapa.keypad(1)
+        self.mapa.timeout(100)
+        
+        self.border = self.screen.derwin(self.mh + 4, self.mw + 8, offset_y - 2, offset_x - 4)
+        
+        self.border.box()
+        self.border.refresh()
+        
+    def draw_field(self):
+        for i, row in enumerate(self.matrix):
+            for j, char in enumerate(row):
+                if self.matrix[i][j] == '█' or self.matrix[i][j] == '-':
+                    self.mapa.addstr(i, j, char, self.color_field)
+                else:
+                    self.mapa.addstr(i, j, char)
+                
+    
+    def score_update(self, score):
+        self.border.addstr(self.mh + 3, 
+            20, 
+            '[ Score: {0} ]'.format(score)
+        )
+        self.border.refresh()
+        
+    def lives_update(self, lives):
+        heart_str = '\u2665 ' * lives
+        
+        line = '[ {0}]─────'.format(heart_str)
+        self.border.addstr(
+            self.mh + 3, 
+            35, 
+            line
+        )
+        
+        self.border.addstr(
+            self.mh + 3, 
+            37, 
+            heart_str,
+            self.color_lives
+        )
+        self.border.refresh()
+        
---
+++ objects/pacman.py
@@ -0,0 +1,94 @@
+import curses
+
+class Pacman:
+
+    def __init__(self, field, position, color):
+    
+        self.y, self.x = position
+        self.field = field
+        self.color = color
+        self.prev_y = self.y
+        self.prev_x = self.x
+        
+        self.pacman_symbols = {
+            'UP': 'v', 
+            'DOWN': '^',
+            'LEFT': '>',
+            'RIGHT': '<'
+        }
+        
+    def move(self, direction):
+        
+        self.prev_y = self.y   #сохраняем текущие координаты
+        self.prev_x = self.x
+        direct = direction
+        
+        
+        if direct == 'UP':
+            self.y -= 1
+        elif direct == 'DOWN':       #изменяем координаты в соответствии с направлением
+            self.y += 1
+        elif direct == 'LEFT':
+            self.x -= 1
+        elif direct == 'RIGHT':
+            self.x += 1
+            
+        width = len(self.field.matrix[0]) #проход
+        if self.x < 0:
+            self.x = width - 1   # влево
+        elif self.x >= width - 1:   #вправо
+            self.x = 1
+            
+        if self.field.matrix[self.y][self.x] == '█' or self.field.matrix[self.y][self.x] == '-':  #если новые координаты - стена, возвращаем первоначальные 
+            self.y = self.prev_y
+            self.x = self.prev_x
+            #self.stopped = True
+        else:
+            self.draw(self.prev_y, self.prev_x, direction)        # если не стена -> нужно отрисовать в новых координатах и стереть из предыдущих
+            
+        
+    def set_character(self, position):
+    
+        self.disappear()
+        self.y, self.x = position
+        direction = 'RIGHT'
+        self.field.mapa.addch(   
+            self.y, 
+            self.x, 
+            self.pacman_symbols[direction],
+            self.color
+        )
+            
+        self.field.mapa.refresh()
+        
+    def disappear(self):
+        clear = ' '
+        self.field.mapa.addch(   
+                self.y, 
+                self.x, 
+                clear
+            )
+        self.field.mapa.refresh()
+            
+    
+    def draw(self, prev_y, prev_x, direction):
+    
+        clear = ' '
+        
+        self.field.mapa.addch(   #стираем пакмана с предыдущей позиции
+            prev_y, 
+            prev_x, 
+            clear
+        )
+        
+        self.field.mapa.addch(   #рисуем на новой
+            self.y, 
+            self.x, 
+            self.pacman_symbols[direction],
+            self.color
+        )
+            
+        self.field.mapa.refresh()
+        
+    
+        
